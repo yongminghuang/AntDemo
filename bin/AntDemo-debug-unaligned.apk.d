88deebc51116e35d9b3bf91baa725eda5c5d082e
yongminghuang <hym@huangyongmingdeMacBook-Air.local>
2017-02-24 09:11:02 +0800

    删除github警告的apk

--- bin/AntDemo-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/huangym/Downloads/sort/AntDemo/AntDemo/bin/AntDemo-debug-unaligned.apk : \
-/Users/huangym/Downloads/sort/AntDemo/AntDemo/bin/AntDemo.ap_ \
-/Users/huangym/Downloads/sort/AntDemo/AntDemo/bin/classes.dex \
-/Users/huangym/Downloads/sort/AntDemo/AntDemo/libs/android-support-v4.jar \
